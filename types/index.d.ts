6f988271b100094a6b5b55d8dfb3b205e5d2580c
bensku <bmi@iki.fi>
2020-10-24 00:12:03 +0300

    Paper type generation pipeline

---
+++ types/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="co_aikar.d.ts" />
+/// <reference path="com_destroystokyo.d.ts" />
+/// <reference path="io_papermc.d.ts" />
+/// <reference path="net_md_5.d.ts" />
+/// <reference path="org_bukkit.d.ts" />
+/// <reference path="org_spigotmc.d.ts" />
